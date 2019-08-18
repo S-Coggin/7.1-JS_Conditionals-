81dcc332b75b4602871c409b04fd6775937d8108
S-Coggin <53793753+S-Coggin@users.noreply.github.com>
2019-08-18 14:54:48 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+alert("CONNECTED");
+var age = prompt("How old are you?");
+
+if (age < 1) {
+    console.log("Error");
+} else if(age = 21) {
+    console.log("Happy 21st Birthday!!");
+} else if(age % !2== 0) {
+    console.log("your age is odd");
+} else if(age % 2 == 0);
+}
